4541a3cc7c4492be7b5c62b861c55f2793c9cac3
fiatjaf <fiatjaf@gmail.com>
2023-04-05 09:19:51 -0300

    sw: decode and better info.

--- sw/src/main/scala/Commands.scala
+++ sw/src/main/scala/Commands.scala
@@ -3,6 +3,7 @@ import cats.data.Validated
 import cats.effect.IO
 import cats.effect.std.Console
 import com.monovore.decline.*
+import io.circe.*
 import io.circe.generic.auto.*
 import io.circe.syntax.*
 import scodec.bits.*
@@ -12,6 +13,7 @@ import soma.*
 sealed trait CommandMethod
 case class Invalid(reason: String) extends CommandMethod
 case object Info extends CommandMethod
+case class Decode(stuff: String) extends CommandMethod
 case object Mint extends CommandMethod
 case class Send(asset: Int, counter: Int, to: XOnlyPublicKey)
     extends CommandMethod
@@ -24,6 +26,11 @@ object Commands {
     Opts.subcommands(
       Command("info", "Displays wallet info.") {
         Opts.unit.map(_ => Info)
+      },
+      Command("decode", "Decodes a transaction.") {
+        Opts
+          .argument[String]("tx")
+          .map(tx => Decode(tx))
       },
       Command("mint", "Mint a new asset.") {
         Opts.unit.map(_ => Mint)
@@ -65,12 +72,33 @@ object Commands {
       case Left(help)                => Console[IO].errorln(help).as(1)
       case Right(Invalid(message))   => Console[IO].errorln(message).as(2)
       case Right(Info)               => displayInfo().as(0)
+      case Right(Decode(stuff))      => decodeStuff(stuff).as(0)
       case Right(Mint)               => mintNewAsset().as(0)
       case Right(Send(asset, c, to)) => sendAsset(asset, c, to).as(0)
     }
 
-  def displayInfo()(implicit config: Config): IO[Unit] =
-    Console[IO].println(config.asJson.spaces2)
+  def displayInfo()(implicit config: Config): IO[Unit] = {
+    val info = JsonObject(
+      "privkey" -> config.priv.value.toHex.asJson,
+      "pubkey" -> config.pub.toHex.asJson
+    )
+    Console[IO].println(info.asJson.spaces2)
+  }
+
+  def decodeStuff(stuff: String)(implicit config: Config): IO[Unit] =
+    BitVector.fromHex(stuff).flatMap(Tx.codec.decodeValue(_).toOption) match {
+      case None => Console[IO].errorln(s"invalid tx: $stuff")
+      case Some(tx) => {
+        val info = JsonObject(
+          "asset" -> tx.asset.asJson,
+          "to" -> tx.to.toHex.asJson,
+          "from" -> tx.from.toHex.asJson,
+          "counter" -> tx.counter.asJson,
+          "signature" -> tx.signature.bytes.toHex.asJson
+        )
+        Console[IO].println(info.asJson.spaces2)
+      }
+    }
 
   def mintNewAsset()(implicit config: Config): IO[Unit] =
     Console[IO].println(
